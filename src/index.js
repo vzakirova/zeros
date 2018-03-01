1e8f3d5346931a2d1d168fce3c9af4bb9da6a501
vzakirova <veranika.zakirava@gmail.com>
2018-03-01 03:47:02 +0300

    to lose it aaaaaaall

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+  var count = 0;
+
+  while (number > 0)
+  {
+    if (Math.floor(number/5)>0)
+    {
+      count += Math.floor(number/5);
+      number /= 5;
+    }
+    else break;
+  }
+
+  return count;
 }
